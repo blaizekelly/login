c5fc60a603c2485641eba5deb8f798b18cc6df5f
Username <username@Usernames-MacBook-Pro.local>
2021-12-01 14:40:17 +0100

    initial commit

--- src/helpers/firebaseConfig.js
+++ src/helpers/firebaseConfig.js
@@ -4,12 +4,7 @@ import 'firebase/compat/auth'
 import 'firebase/compat/firestore'
 
   const firebaseConfig = {
-    apiKey: "AIzaSyD_ADYolN0_71NdtK6FEo4xUOLaepF85g8",
-    authDomain: "bioartagger.firebaseapp.com",
-    projectId: "bioartagger",
-    storageBucket: "bioartagger.appspot.com",
-    messagingSenderId: "416358619193",
-    appId: "1:416358619193:web:b78a86984cf813caadd5e8"
+
 };
 firebase.initializeApp(firebaseConfig);
 
